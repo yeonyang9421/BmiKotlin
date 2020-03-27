3af80e210ff36e236c6498789b4aee65f52b768a
Yeonyang9421 <yeonyang21@gmail.com>
2020-03-27 17:53:05 +0900

    brench test7

--- app/src/main/java/kr/co/woobi/imyeon/bmikotlin/MainActivity.kt
+++ app/src/main/java/kr/co/woobi/imyeon/bmikotlin/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
-//brench test6
+//brench test7
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
